e6acbbd50ceb852ccb2313b61a7242e31942df53
İsmayilzade <ismayilzade376@gmail.com>
2026-07-20 22:33:22 +0400

    email regex

--- interactivity.js
+++ interactivity.js
@@ -1,15 +1,23 @@
-document.querySelectorAll('a').forEach(link =>{
+document.querySelectorAll('a[href^="#"]').forEach(link =>{
     link.addEventListener('click',function(e){
         e.preventDefault()
         document.querySelector(this.getAttribute('href')).scrollIntoView({behavior:'smooth'})
     })
 })
-document.querySelector('.btn-submit')
-            .addEventListener('click',function(e) {e.preventDefault()
-        })
 
+const alert = document.querySelector('.alert')
+document.querySelector('.order-form').addEventListener('submit', function(e) {
+    e.preventDefault()
+    alert.display.style = "flex"
+    alert.innerHTML = "Form uğurla göndərildi!"
+})
 
 const barMenu = document.querySelector('.bar-menu')
 document.querySelector('.bars')
-            .addEventListener('click',function(){barMenu.style.display = barMenu.style.display === "none" ? "flex" : "none"
+            .addEventListener('click',function(){barMenu.style.display = barMenu.style.display ===  "flex" ? "none"  : "flex"
+
         })
+
+
+
+   
